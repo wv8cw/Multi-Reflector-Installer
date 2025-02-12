6fc291879640dfa8ca5b1310446bbab4b5276484
wv8cw <48112303+wv8cw@users.noreply.github.com>
2025-02-12 10:17:33 -0600

    Update Multi-Reflector-Installer.sh

--- Multi-Reflector-Installer.sh
+++ Multi-Reflector-Installer.sh
@@ -80,7 +80,7 @@ echo "Installing dependicies..........."
 echo "------------------------------------------------------------------------------"
 apt update
 apt -y install $DEP
-a2enmod php8.2
+a2enmod php8.0
 echo "------------------------------------------------------------------------------"
 #Create the install directory and app directories
 mkdir -p $XLXINTDIR
